12c22e3dfc49d8e3bcab0a50e6b6df756c5df003
Дамир <workgit7@gmail.com>
2024-10-01 15:14:38 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,7 +10,30 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Пркатика 3.2.2.");
             Console.WriteLine("Привет, \nМир!");
+
+
+            Console.WriteLine();
+            Console.WriteLine("Практика 3.2.8");
+            const string MyName = "DaIko";
+            Console.WriteLine("\tПривет, мир!");
+            Console.WriteLine("\t Мне 18 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine('\u0040');
+            Console.WriteLine('\x23');
+
+            Console.WriteLine();
+            Console.WriteLine("Практика 3.3.2");
+            string MyName2 = "DaIKO";
+            byte MyAge = 18;
+            bool HaveIApet = false;
+            double MyShoeSize = 45;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
         }
     }
 }
